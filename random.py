b848abfcb898b67c0aa66f43a6d39f3ed19b7f29
joshuaelamin <81278002+joshuaelamin@users.noreply.github.com>
2021-06-19 13:04:51 -0700

    resolve test

--- random.py
+++ random.py
@@ -2,21 +2,13 @@
 dummy file
 '''
 
-<<<<<<< HEAD
-import pandas as p
-=======
 import pandas as pand
->>>>>>> 8c5cd5a4e685e2beac5881635b14fe8b5c9f3914
 
 
 list_file = open("List", "wb")
 dataframe_file = open("dataframe", "wb")
 
-<<<<<<< HEAD
-dataframe = p.DataFrame()
-=======
 dataframe = pand.DataFrame()
->>>>>>> 8c5cd5a4e685e2beac5881635b14fe8b5c9f3914
 
 list_values = [1,2,3]
 
