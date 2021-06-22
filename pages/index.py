c918ac20791fe39a89f42769436e89e1f4ac1ed4
andrewlee977 <andrew.lee97@gmail.com>
2021-06-21 22:02:52 -0700

    add landing page image

--- pages/index.py
+++ pages/index.py
@@ -27,13 +27,9 @@ column1 = dbc.Col(
     md=4,
 )
 
-gapminder = px.data.gapminder()
-fig = px.scatter(gapminder.query("year==2007"), x="gdpPercap", y="lifeExp", size="pop", color="continent",
-           hover_name="country", log_x=True, size_max=60)
-
 column2 = dbc.Col(
     [
-        dcc.Graph(figure=fig),
+        html.Img(src="assets/airbnb_image.jpeg", width='725px', height='400px')
     ]
 )
 
