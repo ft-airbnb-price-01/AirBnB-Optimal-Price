5aad3a401bb13e9fdd46bbb593f4357144d4c142
andrewlee977 <andrew.lee97@gmail.com>
2021-06-25 09:44:24 -0700

    prediction.py docstring

--- pages/prediction.py
+++ pages/prediction.py
@@ -3,10 +3,12 @@
 import tensorflow as tf
 
 
-
 def make_prediction(input_arr):
+    """
+    Takes in 2D numpy array of user input
+    Loads pretrained model and predicts based on user input
+    Returns predicted price per night of listing.
+    """
     nn_model = tf.keras.models.load_model('brute_force_grid_search_model/')
     prediction = nn_model.predict(input_arr)
     return prediction[0]
-
-
